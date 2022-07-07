bb1e85aa936b989616ff04ffae89a97335ab76c8
Chandler May <chandler.camille.may@gmail.com>
2022-07-07 21:26:05 +0000

    Add stub web interface.

---
+++ demo/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import PromptForm from './components/PromptForm.vue'
+
+export default {
+  components: {
+    PromptForm,
+  },
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    onChange(text) {
+      this.text = text;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>
+        You did it! &#x1F495;
+      </h1>
+    </header>
+  
+    <main>
+      <PromptForm :text="text" @change="onChange" />
+    </main>
+  </div>
+</template>
+
+<style src="bootstrap/dist/css/bootstrap.min.css">
+</style>
+
+<style src="bootstrap-icons/font/bootstrap-icons.css">
+</style>
---
+++ demo/src/components/PromptForm.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    text: { required: true, type: String },
+  },
+  emits: ['change'],
+  data() {
+    return {
+      myText: '',
+    };
+  },
+  watch: {
+    text(newValue, oldValue) {
+      this.myText = newValue;
+    },
+  },
+}
+</script>
+
+<template>
+  <form @submit.prevent="$emit('change', myText)">
+    <div class="my-3">
+      <label class="form-label">Text to complete</label>
+      <textarea class="form-control" v-model="myText" />
+    </div>
+    <div class="my-3">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</template>
