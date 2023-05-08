60b1cccfd70f864bae16826e51b50dd1a13a7245
Chandler May <chandler.camille.may@gmail.com>
2023-05-08 14:54:22 -0400

    back to default python

--- backend-deepspeed/docker-command.bash
+++ backend-deepspeed/docker-command.bash
@@ -2,7 +2,5 @@
 set -e
 set -u
 
-CONDA_RUN="/opt/anaconda3/bin/conda run --no-capture-output"
-
-$CONDA_RUN ds_report
-$CONDA_RUN uvicorn --host 0.0.0.0 "$@" main:app
+ds_report
+uvicorn --host 0.0.0.0 "$@" main:app
